2eb517449a0ca376083cb9e3661d6a94c2c00863
Georg Kollegger <gkollegger5@outlook.com>
2024-09-25 11:17:16 +0200

    Added basic login / singup-ui.

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+export default function Signup() {
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <input placeholder="Username" type="text"></input>
+                <input placeholder="Email" type="email"></input>
+                <input placeholder="Password" type="password"></input>
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    )
+}
