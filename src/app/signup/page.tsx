1972f8deef0671c6767e616e36bab6ae24131cc9
TheDuggy <gkollegger5@outlook.com>
2024-09-26 05:38:19 +0200

    Started to add signup-api-route.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,25 @@
+'use client'
+import { useState } from "react";
+
 export default function Signup() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const res = await fetch('/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email, password: password })
+        })
+    }
+
     return (
         <div>
             <h1>Signup</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input placeholder="Username" type="text"></input>
                 <input placeholder="Email" type="email"></input>
                 <input placeholder="Password" type="password"></input>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,18 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { config } from "dotenv";
 
-export function createUser(user:string, email:string) {
+config()
 
+const auth = getAuth(initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+}))
+
+export function createUser(email: string, password: string) {
+    createUserWithEmailAndPassword(auth, email, password);
 }
