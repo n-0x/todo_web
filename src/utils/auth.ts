0c4b4bafd4e097720de66d54c873ac921401aa96
TheDuggy <gkollegger5@outlook.com>
2024-09-29 20:43:31 +0200

    Added missing await.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -5,7 +5,7 @@ import { user } from "./db";
 export async function createUser(username: string, email: string, password: string): Promise<void> {
     const pass_salt: string = await genSalt(Number.parseInt(process.env.SALT_ROUNDS as string));
     const pass_hash = await hash(password, pass_salt);
-    user.create({
+    await user.create({
         data: {
             username: username,
             email: email,
